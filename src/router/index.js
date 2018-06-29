32845d24df6924015b2fd23e9140a3bb438005b3
Calinas <1607731780@qq.com>
2018-06-29 13:44:30 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import EcommerceIndex from '@/pages/ecommerce/EcommerceIndex'
-import GoodsDetailPage from '@/pages/ecommerce/goodsDetail'
-import Index from '@/pages/index'
-import ActivityIndex from '@/pages/activity/ActivityIndex'
-import WeibaIndex from '@/pages/weiba/WeibaIndex'
-import WeibaDetail from '@/pages/weiba/detail'
-import ActivityDetail from '@/pages/activity/detail'
-import CourseIndex from '@/pages/course/index'
-import CourseDetail from '@/pages/course/detail'
-import CityList from '@/pages/activity/cityList'
-import ShoppingCart from '@/pages/ecommerce/shoppingcart'
-import Login from '@/pages/user/login'
-import GoodsList from '@/pages/ecommerce/goodsList'
+// import EcommerceIndex from '@/pages/ecommerce/EcommerceIndex'
+// import GoodsDetailPage from '@/pages/ecommerce/goodsDetail'
+// import Index from '@/pages/index'
+// import ActivityIndex from '@/pages/activity/ActivityIndex'
+// import WeibaIndex from '@/pages/weiba/WeibaIndex'
+// import WeibaDetail from '@/pages/weiba/detail'
+// import ActivityDetail from '@/pages/activity/detail'
+// import CourseIndex from '@/pages/course/index'
+// import CourseDetail from '@/pages/course/detail'
+// import CityList from '@/pages/activity/cityList'
+// import ShoppingCart from '@/pages/ecommerce/shoppingcart'
+// import Login from '@/pages/user/login'
+// import GoodsList from '@/pages/ecommerce/goodsList'
 
 
 Vue.use(Router)
+
+const EcommerceIndex = () => import('@/pages/ecommerce/EcommerceIndex')
+const GoodsDetailPage = () => import('@/pages/ecommerce/goodsDetail')
+const Index = () => import('@/pages/index')
+const ActivityIndex = () => import('@/pages/activity/ActivityIndex')
+const WeibaIndex = () => import('@/pages/weiba/WeibaIndex')
+const WeibaDetail = () => import('@/pages/weiba/detail')
+const ActivityDetail = () => import('@/pages/activity/detail')
+const CourseIndex = () => import('@/pages/course/index')
+const CourseDetail = () => import('@/pages/course/detail')
+const CityList = () => import('@/pages/activity/cityList')
+const ShoppingCart = () => import('@/pages/ecommerce/shoppingcart')
+const Login = () => import('@/pages/user/login')
+const GoodsList = () => import('@/pages/ecommerce/goodsList')
+
+
 
 export default new Router({
     mode:'history',
